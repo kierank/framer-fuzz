632a46de1e9fcde8a32e3cac12985477885d0919
Judah Rand <judahrand@obe.tv>
2017-11-17 14:16:29 +0000

    no message

--- framer-fuzz.c
+++ framer-fuzz.c
@@ -177,7 +177,7 @@ int main(int argc, char **argv)
         uref_sound_flow_set_raw_sample_size(flow_def, 20);
 
         struct upipe_mgr *upipe_block_to_sound_mgr = upipe_block_to_sound_mgr_alloc();
-        struct upipe *upipe_block_to_sound  = upipe_void_alloc_output(upipe_src,
+        struct upipe *upipe_block_to_sound  = upipe_flow_alloc_output(upipe_src,
                        upipe_block_to_sound_mgr, uprobe_pfx_alloc(uprobe_use(uprobe),
                        UPROBE_LOG_DEBUG, "block_to_sound"), flow_def);
         assert(upipe_block_to_sound != NULL);
